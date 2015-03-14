c9bbd66a4ebd815e1745d84a258b7a15d81a163d
Eldar Khaitov <benzin.a95@list.ru>
2015-03-14 19:36:10 +0200

    Update sideMenus.js

--- htmls/sideMenus/sideMenus.js
+++ htmls/sideMenus/sideMenus.js
@@ -10,7 +10,12 @@ var htmls = {forks:'\
 	</a>\
 </div>',
 grips:null,
-handlebars:null,
+handlebars:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=seatpost_stem_seized.html">\
+		Удаление прикипевших глаголов и выносов\
+	</a>\
+</div>',
 headsets:'\
 <div class="list-group">\
 	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=headet_standards.html">\
@@ -19,9 +24,126 @@ headsets:'\
 	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=conventional_threadless.html">\
 		Обслуживание безрезьбовых колонок\
 	</a>\
-</div>'};
+</div>',
+
+//-------------------------------------------------------------------------------------------------------------
+
+hubs:null,
+spokes:null,
+rims:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=wheel_building.html">\
+		Как собрать велосипедное колесо\
+	</a>\
+</div>',
+tires:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=inner_tube_repair.html">\
+		Ремонт велосипедной камеры и покрышки\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=tire_tube_removal_installaton.html">\
+		Установка и удаление покрышки и камеры\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=ust_tubeless.html">\
+		Установка и обслуживание бескамерных покрышек\
+	</a>\
+</div>'
+
+//-------------------------------------------------------------------------------------------------------------
+
+
+pedals:null,
+cranksets'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=isis_octalink_removal_installation.html">\
+		Установка и удаление шатунов: ISIS и Octalink\
+	</a>\
+</div>',
+cassetes:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=cassette_freewheel_remove.html">\
+		Установка и удаление кассеты\
+	</a>\
+</div>',
+chains:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=chain_install.html">\
+		Как собрать велосипедное колесо\
+	</a>\
+</div>',
+
+//-------------------------------------------------------------------------------------------------------------
+
+shifters:null,
+brakes:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=inner_tube_repair.html">\
+		Ремонт велосипедной камеры и покрышки\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=tire_tube_removal_installaton.html">\
+		Установка и удаление покрышки и камеры\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=ust_tubeless.html">\
+		Установка и обслуживание бескамерных покрышек\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=ust_tubeless.html">\
+		Установка и обслуживание бескамерных покрышек\
+	</a>\
+</div>',
+frontDerailleurs:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=fd_adjust.html">\
+		Настройка переднего переключателя\
+	</a>\
+</div>',
+rearDerailleurs:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=rd_adjust.html">\
+		Настройка заднего переключателя\
+	</a>\
+</div>',
+rigEtc:null,
+
+//-------------------------------------------------------------------------------------------------------------
+
+bikes:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=bike_history.html">\
+		История развития велосипеда\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=recumbent.html">\
+		История развития рикамбента\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=mtb.html">\
+		История развития горного велосипеда\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=bmx.html">\
+		История развития BMX\
+	</a>\
+</div>',
+frames:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=frame_materials.html">\
+		Велосипедная рама – из чего она сделанна\
+	</a>\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=aluminium_frames.html">\
+		Материалы велосипедных рам. Алюминиевые сплавы\
+	</a>\
+</div>',
+saddles:'\
+<div class="list-group">\
+	<a class="list-group-item" href="' + url + '?cat=' + cat + '&art=seatpost_stem_seized.html">\
+		Удаление прикипевших глаголов и выносов\
+	</a>\
+</div>',
+
+//-------------------------------------------------------------------------------------------------------------
+
+electronics:null,
+carriers:null,
+flasks:null,
+auxilliariesEtc:null};
 
 console.log('url, cat: ' + url + ' ' + cat);
 
 //-------------------------------------------------------------------------------------------------------------
-
