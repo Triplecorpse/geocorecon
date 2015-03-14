a9df0eb45ce155ece535f6c9eeea0e21da072db3
Eldar Khaitov <benzin.a95@list.ru>
2015-03-14 18:18:20 +0200

    Create sidemenus.js

---
+++ htmls/sideMenus/sidemenus.js
@@ -0,0 +1,25 @@
+//Retrieving html to paste into links
+var url = document.location.href;
+url = url.slice(0, url.indexOf("?"));
+console.log(url);
+
+var htmls = {forks:'\
+<div class="list-group">\
+	<a class="list-group-item" href="&art=steering_column_length.html">\
+		Обрезка рулевой трубы вилки\
+	</a>\
+</div>',
+grips:null,
+handlebars:null,
+headsets:'\
+<div class="list-group">\
+	<a class="list-group-item" href="&art=headet_standards.html">\
+		Стандарты рулевых колонок\
+	</a>\
+	<a class="list-group-item" href="&art=conventional_threadless.html">\
+		Обслуживание безрезьбовых колонок\
+	</a>\
+</div>'}
+
+//-------------------------------------------------------------------------------------------------------------
+
