2acc459d479098b75493854f6371a363a86e29bf
Eldar Khaitov <benzin.a95@list.ru>
2015-03-18 23:49:21 +0200

    Update converter.js

--- js/converter.js
+++ js/converter.js
@@ -7,14 +7,14 @@
 //this function converts from ddºmm'ss" to all others
 function fromDdmmss(dd, mm, ss)
 {
-    gddPointD8 = dd / 1 + mm / 60 + ss / 3600;
+    gddPointD8 = (dd / 1 + mm / 60 + ss / 3600).toPrecision(5);
     fromDdPointD8(gddPointD8);
 }
 
 //this function converts from ddºmm.mmmmmmmm to all others
 function fromDdmmPointM8(dd, mmPointM8)
 {
-    gddPointD8 = dd / 1 + mmPointM8 / 60;
+    gddPointD8 = dd / 1 + mmPointM8 / 60.toPrecision(5);
     fromDdPointD8(gddPointD8);
 }
 
@@ -22,8 +22,8 @@ function fromDdmmPointM8(dd, mmPointM8)
 function fromDdPointD8(ddPointD8)
 {
     var dd = truncate(ddPointD8);
-    var mm = (ddPointD8 - dd) * 60;
-    var ss = ((ddPointD8 - dd) * 60 - mm) * 60;
+    var mm = ((ddPointD8 - dd) * 60).toPrecision(5);
+    var ss = (((ddPointD8 - dd) * 60 - mm) * 60).toPrecision(5);
     gddmmss = dd + "°" + truncate(mm) + "'" + ss + '"';
     gddmmPointM8 = dd + "°" + mm + "'";
 }
@@ -34,9 +34,4 @@ function truncate(number)
     return number > 0
         ? Math.floor(number)
         : Math.ceil(number);
-}
-
-function chooseConverter()
-{
-
 }
