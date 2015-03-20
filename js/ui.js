94d26d2b2e0d0964dff14aaefceb6df72a7255ea
Eldar Khaitov <benzin.a95@list.ru>
2015-03-20 19:02:51 +0200

    Update ui.js

--- js/ui.js
+++ js/ui.js
@@ -9,29 +9,29 @@
 //document.getElementById("dddd").setAttribute("pattern","^[+,-]?\\d{1,3}[.]\\d+[°]?\\s[+,-]?\\d{1,3}[.]\\d+[°]?$");
 function inputChange(id)
 {
-    if((id == "ddmmss") && (document.getElementById("ddmmss").value != ""))
+    //if((id == "ddmmss") && (document.getElementById("ddmmss").value != ""))
     {
-        document.getElementById("ddmmmm").disabled = true;
-        document.getElementById("dddd").disabled = true;
+        //document.getElementById("ddmmmm").disabled = true;
+        //document.getElementById("dddd").disabled = true;
     }
-    else if((id == "ddmmmm") && (document.getElementById("ddmmmm").value != ""))
+    //else if((id == "ddmmmm") && (document.getElementById("ddmmmm").value != ""))
     {
-        document.getElementById("ddmmss").disabled = true;
-        document.getElementById("dddd").disabled = true;
+        //document.getElementById("ddmmss").disabled = true;
+        //document.getElementById("dddd").disabled = true;
     }
-    else if((id == "dddd") && (document.getElementById("dddd").value != ""))
+    //else if((id == "dddd") && (document.getElementById("dddd").value != ""))
     {
-        document.getElementById("ddmmss").disabled = true;
-        document.getElementById("ddmmmm").disabled = true;
+        //document.getElementById("ddmmss").disabled = true;
+        //document.getElementById("ddmmmm").disabled = true;
     }
-    else
+    //else
     {
-        document.getElementById("ddmmss").disabled = false;
-        document.getElementById("ddmmmm").disabled = false;
-        document.getElementById("dddd").disabled = false;
+        //document.getElementById("ddmmss").disabled = false;
+        //document.getElementById("ddmmmm").disabled = false;
+        //document.getElementById("dddd").disabled = false;
 
-        document.getElementById("ddmmss").value = "";
-        document.getElementById("ddmmmm").value = "";
-        document.getElementById("dddd").value = "";
+        //document.getElementById("ddmmss").value = "";
+        //document.getElementById("ddmmmm").value = "";
+        //document.getElementById("dddd").value = "";
     }
 }
